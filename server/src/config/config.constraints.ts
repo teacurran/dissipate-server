a710baf2a77490f0aa2aa53cadf7e5b273c628e3
Terrence Curran <tea@approachingpi.com>
2022-05-24 16:26:11 -0400

    changed ghost post to dissipate

--- server/src/config/config.constraints.ts
+++ server/src/config/config.constraints.ts
@@ -1,5 +1,5 @@
-import { ClientOpts } from 'redis';
-import { Options } from 'sequelize';
+import {ClientOpts} from 'redis'
+import {Options} from 'sequelize'
 
 /**
  * this file is here to get a typed environment
@@ -14,22 +14,6 @@ export type EnvironmentType =
 export interface IConfigApp {
   database: IDatabaseConfiguration;
   redis: ClientOpts;
-
-  storage: {
-    operations: string;
-    staticData: string;
-    shareData: string;
-    userData: string;
-  };
-
-  serviceAccount: {
-    email: string;
-  };
-
-  frontend_url: string;
-  iap_client_id?: string;
-  api_key: string;
-  appengine_region_code: string;
 }
 
 interface IDatabaseConfiguration extends Options {
--- server/src/config/dockerCompose.ts
+++ server/src/config/dockerCompose.ts
@@ -1,35 +1,18 @@
-import { IConfigApp } from './config.constraint';
+import {IConfigApp} from './config.constraints'
 
 /** Used when running app inside docker-compose, pointing to database docker */
 export const config: IConfigApp = {
   database: {
-    host: process.env.POSTGRES_HOST || 'restor-db',
-    port: parseInt(process.env.POSTGRES_PORT) || 5432,
-    username: 'restor',
-    password: 'restor',
-    database: process.env.POSTGRES_DB || 'restor',
+    host: process.env.POSTGRES_HOST || 'dissipate-db',
+    port: parseInt(process.env.POSTGRES_PORT || '') || 5432,
+    username: 'dissipate',
+    password: 'dissipate',
+    database: process.env.POSTGRES_DB || 'dissipate',
     dialect: 'postgres',
-    environment: 'dockerCompose'
+    environment: 'dockerCompose',
   },
   redis: {
     host: process.env.REDIS_HOST || 'redis',
-    port: parseInt(process.env.REDIS_PORT) || 6379
+    port: parseInt(process.env.REDIS_PORT || '') || 6379,
   },
-  storage: {
-    operations: 'gweb-restor-dev-earthengine-tasks-cache',
-    tileset: 'restor-dev-tilesets',
-    staticData: 'restor-assets',
-    shareData: 'gweb-restor-dev-share-data',
-    userData: 'gweb-restor-dev-user-data'
-  },
-  serviceAccount: {
-    email: 'earthengine-service-account@gweb-restor-dev.iam.gserviceaccount.com'
-  },
-  // frontend_url: 'http://web:3000'
-  frontend_url: 'https://gweb-restor-dev.uc.r.appspot.com',
-  iap_client_id:
-    process.env.IAP_CLIENT_ID ||
-    '114750871121-9spqf94risipst5s0kfejgp94fr25o36.apps.googleusercontent.com',
-  api_key: 'AIzaSyD0WGhPp1k3vajw_lYTTI2tGYtd8BEOFaI',
-  appengine_region_code: 'oa'
-};
+}
