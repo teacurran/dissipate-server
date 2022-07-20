45bb5f971d89dead1b24ceb34b2ab0ba745d628b
Terrence Curran <tea@approachingpi.com>
2022-07-19 23:44:47 -0400

    still getting set up

---
+++ api/src/main/java/app/dissipate/UserResource.java
@@ -0,0 +1,18 @@
+package app.dissipate;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/users")
+public class UserResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from dissipate!";
+    }
+
+
+}
