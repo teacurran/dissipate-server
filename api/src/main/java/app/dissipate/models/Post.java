5d73ef8ad059e189f826102dfdf90f26f4c45b07
Terrence Curran <tcurran@approachingpi.com>
2022-09-08 01:08:34 -0400

    not much

---
+++ api/src/main/java/app/dissipate/models/Post.java
@@ -0,0 +1,10 @@
+package app.dissipate.models;
+
+import io.quarkus.hibernate.reactive.panache.PanacheEntity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Post extends PanacheEntity {
+
+}
