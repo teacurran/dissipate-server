bd7ff73536f81815543874c688e1f0c92ef1852e
Terrence Curran <tcurran@approachingpi.com>
2022-11-21 10:17:20 -0500

    looking at tracer injection

--- api/src/main/java/app/dissipate/workflows/CrawlDomainWorkflowImpl.java
+++ api/src/main/java/app/dissipate/workflows/CrawlDomainWorkflowImpl.java
@@ -1,10 +1,13 @@
 package app.dissipate.workflows;
 
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.instrumentation.annotations.SpanAttribute;
 import io.opentelemetry.instrumentation.annotations.WithSpan;
 import io.quarkus.temporal.runtime.annotations.TemporalActivityStub;
 import io.quarkus.temporal.runtime.annotations.TemporalWorkflow;
+
+import javax.inject.Inject;
 
 @TemporalWorkflow(queue = "crawl-domain", name = "crawl-domain")
 public class CrawlDomainWorkflowImpl implements CrawlDomainWorkflow {
@@ -12,15 +15,21 @@ public class CrawlDomainWorkflowImpl implements CrawlDomainWorkflow {
     @TemporalActivityStub
     CrawlDomainCheckActivity crawlDomainCheckActivity;
 
+    @Inject
+    Tracer tracer;
+
     @WithSpan("crawl-domain")
     @Override
     public void crawlDomain(@SpanAttribute(value = "domain") String domain) {
-        Span.current().setAttribute("domain", domain);
+        Span span = tracer.spanBuilder("crawl-domain").startSpan();
+        span.setAttribute("domain", domain);
 
         if (crawlDomainCheckActivity.checkDomain(domain)) {
             System.out.println("Domain " + domain + " is valid");
         } else {
             System.out.println("Domain " + domain + " is invalid");
         }
+
+        span.end();
     }
 }
