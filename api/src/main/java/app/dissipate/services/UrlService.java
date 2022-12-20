6dd215f2caa48d2c7e8ea0ec9da3ce7fa4802587
Terrence Curran <tcurran@approachingpi.com>
2022-12-20 01:01:51 -0500

    trying to get spans in message receiver

--- api/src/main/java/app/dissipate/services/UrlService.java
+++ api/src/main/java/app/dissipate/services/UrlService.java
@@ -2,8 +2,11 @@ package app.dissipate.services;
 
 import app.dissipate.data.cassandra.dao.UrlDao;
 import app.dissipate.data.cassandra.models.Url;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
 import io.smallrye.common.annotation.Blocking;
-import io.smallrye.mutiny.Multi;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
@@ -22,6 +25,9 @@ public class UrlService {
     @Inject
     UrlDao urlDao;
 
+    @Inject
+    Tracer tracer;
+
     public Url addUrl(final String value) {
         Url url = new Url(value);
         url.setDateCreated(Instant.now());
@@ -34,9 +40,19 @@ public class UrlService {
     @Incoming("url-created-in")
     @Blocking
     @Transactional
-    public void urAdded(Object message) {
+    public void urlAdded(Object message) {
 
-        System.out.println(message);
+        System.out.println("here0");
+        tracer.spanBuilder("url-added")
+                .setAttribute(AttributeKey.stringKey("url"), message.toString())
+                .startSpan()
+                .end();
+        System.out.println("here1" + Span.current().toString());
+//        Span.current().addEvent("processing url", Attributes.of(
+//                AttributeKey.stringKey("url"), message.toString()
+//        ));
+        System.out.println("here2");
+
     }
 
 }
