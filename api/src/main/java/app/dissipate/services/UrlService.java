497cc0e0edb178f4baeb4f31988606e1083ccac2
Terrence Curran <tcurran@approachingpi.com>
2022-12-20 13:30:22 -0500

    otel testing

--- api/src/main/java/app/dissipate/services/UrlService.java
+++ api/src/main/java/app/dissipate/services/UrlService.java
@@ -5,7 +5,9 @@ import app.dissipate.data.cassandra.models.Url;
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Context;
 import io.opentelemetry.instrumentation.annotations.WithSpan;
 import io.smallrye.common.annotation.Blocking;
 import org.eclipse.microprofile.reactive.messaging.Channel;
@@ -41,20 +43,24 @@ public class UrlService {
     @Incoming("url-created-in")
     @Blocking
     @Transactional
-    @WithSpan
     public void urlAdded(Object message) {
 
+        processMesasage(message);
+    }
+
+    public void processMesasage(Object message) {
         System.out.println("here0");
-        tracer.spanBuilder("url-added")
+        Span span = tracer.spanBuilder("url-added")
                 .setAttribute(AttributeKey.stringKey("url"), message.toString())
-                .startSpan()
-                .end();
-        System.out.println("here1" + Span.current().toString());
-        Span.current().addEvent("processing url", Attributes.of(
+                .setParent(Context.current().with(Span.current()))
+                .setSpanKind(SpanKind.INTERNAL)
+                .startSpan();
+        System.out.println("here1: " + Span.current().toString());
+        span.addEvent("processing url", Attributes.of(
                 AttributeKey.stringKey("url"), message.toString()
         ));
         System.out.println("here2");
 
+        span.end();
     }
-
 }
