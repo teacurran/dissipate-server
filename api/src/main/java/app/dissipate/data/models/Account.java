2e90b675ff3acf4d844819ddf831f72d6cadf590
Terrence Curran <tcurran@approachingpi.com>
2024-08-07 10:53:49 -0400

    upgraded quarkus + MaxIntDto

--- api/src/main/java/app/dissipate/data/models/Account.java
+++ api/src/main/java/app/dissipate/data/models/Account.java
@@ -6,11 +6,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name = "accounts")
 public class Account extends DefaultPanacheEntityWithTimestamps {
   public AccountStatus status;
 
--- api/src/main/java/app/dissipate/data/models/dto/MaxIntDto.java
+++ api/src/main/java/app/dissipate/data/models/dto/MaxIntDto.java
@@ -4,9 +4,13 @@ import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @RegisterForReflection
 public class MaxIntDto {
-    public final int maxValue;
+  public final int maxValue;
 
-    public MaxIntDto(int maxValue) {
-        this.maxValue = maxValue;
-    }
+  public MaxIntDto(int maxValue) {
+    this.maxValue = maxValue;
+  }
+
+  public MaxIntDto(Integer maxValue) {
+    this.maxValue = maxValue;
+  }
 }
