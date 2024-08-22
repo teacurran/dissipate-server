00d92c6304ff15d3f50e03a19ecd7cf5342a57bb
Terrence Curran <tcurran@approachingpi.com>
2024-08-22 01:16:59 -0400

    error message on email validation

--- api/src/main/java/app/dissipate/api/grpc/DissipateServiceImpl.java
+++ api/src/main/java/app/dissipate/api/grpc/DissipateServiceImpl.java
@@ -6,6 +6,7 @@ import app.dissipate.data.models.Account;
 import app.dissipate.data.models.AccountEmail;
 import app.dissipate.data.models.Session;
 import app.dissipate.data.models.SessionValidation;
+import app.dissipate.grpc.ApiError;
 import app.dissipate.grpc.CreateHandleRequest;
 import app.dissipate.grpc.CreateHandleResponse;
 import app.dissipate.grpc.DissipateService;
@@ -16,6 +17,7 @@ import app.dissipate.grpc.ValidateSessionRequest;
 import app.dissipate.grpc.ValidateSessionResponse;
 import app.dissipate.interceptors.GrpcAuthInterceptor;
 import app.dissipate.services.DelayedJobService;
+import app.dissipate.services.LocalizationService;
 import app.dissipate.utils.EncryptionUtil;
 import app.dissipate.utils.StringUtil;
 import io.opentelemetry.api.common.AttributeKey;
@@ -28,17 +30,21 @@ import io.quarkus.grpc.RegisterInterceptor;
 import io.quarkus.hibernate.reactive.panache.common.WithSession;
 import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
 import io.smallrye.mutiny.Uni;
+import io.vertx.ext.mail.mailencoder.EmailAddress;
 import jakarta.inject.Inject;
 import org.jboss.logging.Logger;
 
 import java.time.Instant;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 @GrpcService
 @RegisterInterceptor(GrpcAuthInterceptor.class)
 public class DissipateServiceImpl implements DissipateService {
 
   private static final Logger LOGGER = Logger.getLogger(DissipateServiceImpl.class);
+
+  public static final String ERROR_EMAIL_INVALID = "auth.error.email.invalid";
 
   @Inject
   SnowflakeIdGenerator snowflakeIdGenerator;
@@ -48,6 +54,9 @@ public class DissipateServiceImpl implements DissipateService {
 
   @Inject
   DelayedJobService delayedJobService;
+
+  @Inject
+  LocalizationService localizationService;
 
   @Override
   @WithSession
@@ -60,48 +69,64 @@ public class DissipateServiceImpl implements DissipateService {
     try (Scope scope = otel.makeCurrent()) {
       otel.setAttribute("email", email);
       otel.addEvent("register user", Attributes.of(AttributeKey.stringKey("request"), request.toString()));
+      Locale locale = LocaleConverter.fromValue(request.getLocale());
+      otel.setAttribute("locale", locale.toLanguageTag());
 
-      return AccountEmail.findByEmailValidated(email)
-        .onItem()
-        .transformToUni(accountEmail -> {
-          if (accountEmail != null) {
-            LOGGER.infov("email already exists: {0}", email);
-            otel.addEvent("email already exists", Attributes.of(AttributeKey.stringKey("email"), email));
-            return Uni.createFrom().item(RegisterResponse.newBuilder().setResult(RegisterResponseResult.Error).build());
-          }
-          Locale locale = LocaleConverter.fromValue(request.getLocale());
-          otel.setAttribute("locale", locale.toLanguageTag());
-          return Account.createNewAnonymousAccount(locale, email, snowflakeIdGenerator, encryptionUtil).onItem().transformToUni(a -> {
-            Session session = new Session();
-            session.account = a;
-            return session.persistAndFlush().onItem().transformToUni(s -> {
-              SessionValidation sessionValidation = new SessionValidation();
-              sessionValidation.session = s;
-              sessionValidation.id = snowflakeIdGenerator.generate(SessionValidation.ID_GENERATOR_KEY);
-              sessionValidation.email = a.emails.get(0);
-              sessionValidation.token = StringUtil.generateRandomString(6);
-              return sessionValidation.persistAndFlush().onItem().transformToUni(sv -> {
-                return delayedJobService.createDelayedJob(sv).onItem().transformToUni(dj -> {
-                  return Uni.createFrom().item(
-                    RegisterResponse.newBuilder()
-                      .setResult(RegisterResponseResult.EmailSent)
-                      .setSid(s.id.toString())
-                      .build()
+      ResourceBundle i18n = localizationService.getBundle(locale);
 
-                  );
-                });
-              });
-            }).onFailure().call(t -> {
-              otel.addEvent("error creating session", Attributes.of(AttributeKey.stringKey("error"), t.getMessage()));
+      return validateEmail(email).onItem().transformToUni(valid -> {
+        if (!valid) {
+          LOGGER.infov("invalid email: {0}", email);
+          otel.addEvent("invalid email", Attributes.of(AttributeKey.stringKey("email"), email));
+          return Uni.createFrom().item(RegisterResponse.newBuilder().setResult(RegisterResponseResult.Error)
+            .setError(ApiError.newBuilder()
+              .setCode(ERROR_EMAIL_INVALID)
+              .setMessage(i18n.getString(ERROR_EMAIL_INVALID))
+              .build()
+            ).build());
+        }
+
+        return AccountEmail.findByEmailValidated(email)
+          .onItem()
+          .transformToUni(accountEmail -> {
+            if (accountEmail != null) {
+              LOGGER.infov("email already exists: {0}", email);
+              otel.addEvent("email already exists", Attributes.of(AttributeKey.stringKey("email"), email));
               return Uni.createFrom().item(RegisterResponse.newBuilder().setResult(RegisterResponseResult.Error).build());
+            }
+
+            return Account.createNewAnonymousAccount(locale, email, snowflakeIdGenerator, encryptionUtil).onItem().transformToUni(a -> {
+              Session session = new Session();
+              session.account = a;
+              return session.persistAndFlush().onItem().transformToUni(s -> {
+                SessionValidation sessionValidation = new SessionValidation();
+                sessionValidation.session = s;
+                sessionValidation.id = snowflakeIdGenerator.generate(SessionValidation.ID_GENERATOR_KEY);
+                sessionValidation.email = a.emails.get(0);
+                sessionValidation.token = StringUtil.generateRandomString(6);
+                return sessionValidation.persistAndFlush().onItem().transformToUni(sv -> {
+                  return delayedJobService.createDelayedJob(sv).onItem().transformToUni(dj -> {
+                    return Uni.createFrom().item(
+                      RegisterResponse.newBuilder()
+                        .setResult(RegisterResponseResult.EmailSent)
+                        .setSid(s.id.toString())
+                        .build()
+
+                    );
+                  });
+                });
+              }).onFailure().call(t -> {
+                otel.addEvent("error creating session", Attributes.of(AttributeKey.stringKey("error"), t.getMessage()));
+                return Uni.createFrom().item(RegisterResponse.newBuilder().setResult(RegisterResponseResult.Error).build());
+              });
             });
           });
 
-        }).onFailure().call(t -> {
-          LOGGER.error("error registering user", t);
-          otel.addEvent("error registering user", Attributes.of(AttributeKey.stringKey("error"), t.getMessage()));
-          return Uni.createFrom().item(RegisterResponse.newBuilder().setResult(RegisterResponseResult.Error).build());
-        });
+      }).onFailure().call(t -> {
+        LOGGER.error("error registering user", t);
+        otel.addEvent("error registering user", Attributes.of(AttributeKey.stringKey("error"), t.getMessage()));
+        return Uni.createFrom().item(RegisterResponse.newBuilder().setResult(RegisterResponseResult.Error).build());
+      });
     }
   }
 
@@ -129,4 +154,19 @@ public class DissipateServiceImpl implements DissipateService {
       return Uni.createFrom().item(ValidateSessionResponse.newBuilder().setValid(false).build());
     });
   }
+
+  @WithSession
+  public Uni<Boolean> validateEmail(String email) {
+    String trimmedEmail = email.trim();
+    if (trimmedEmail.isEmpty()) {
+      return Uni.createFrom().item(false);
+    }
+    try {
+      EmailAddress emailAddr = new EmailAddress(email);
+      return Uni.createFrom().item(true);
+    } catch (IllegalArgumentException e) {
+      return Uni.createFrom().item(false);
+    }
+  }
 }
+
