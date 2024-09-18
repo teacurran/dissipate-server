91d85192e21b68c179c165331a071a2aca75d878
Terrence Curran <tcurran@approachingpi.com>
2024-09-17 23:32:04 -0400

    streaming chat response

--- api/src/main/java/app/dissipate/api/grpc/GetSessionMethod.java
+++ api/src/main/java/app/dissipate/api/grpc/GetSessionMethod.java
@@ -1,13 +1,8 @@
 package app.dissipate.api.grpc;
 
-import app.dissipate.data.jpa.SnowflakeIdGenerator;
+import app.dissipate.data.models.Session;
 import app.dissipate.grpc.GetSessionRequest;
 import app.dissipate.grpc.GetSessionResponse;
-import app.dissipate.interceptors.GrpcLocaleInterceptor;
-import app.dissipate.services.DelayedJobService;
-import app.dissipate.services.LocalizationService;
-import app.dissipate.utils.EncryptionUtil;
-import io.grpc.Status;
 import io.opentelemetry.instrumentation.annotations.WithSpan;
 import io.quarkus.hibernate.reactive.panache.common.WithSession;
 import io.quarkus.security.identity.CurrentIdentityAssociation;
@@ -16,9 +11,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.jboss.logging.Logger;
 
-import java.util.Locale;
-
-import static app.dissipate.api.grpc.GrpcErrorCodes.AUTH_TOKEN_INVALID;
+import javax.annotation.security.RolesAllowed;
 
 @ApplicationScoped
 public class GetSessionMethod {
@@ -26,31 +19,20 @@ public class GetSessionMethod {
   private static final Logger LOGGER = Logger.getLogger(GetSessionMethod.class);
 
   @Inject
-  DelayedJobService delayedJobService;
-
-  @Inject
-  LocalizationService localizationService;
-
-  @Inject
-  SnowflakeIdGenerator snowflakeIdGenerator;
-
-  @Inject
-  EncryptionUtil encryptionUtil;
-
-  @Inject
   CurrentIdentityAssociation identity;
 
   @WithSpan("GetSessionMethod.handler")
+  @RolesAllowed("user")
   @WithSession
   public Uni<GetSessionResponse> handler(GetSessionRequest request) {
     return identity.getDeferredIdentity().onItem().transformToUni(si -> {
-      LOGGER.info("GetSessionMethod.handler: " + si.getPrincipal());
-      if (si.isAnonymous()) {
-        Locale locale = GrpcLocaleInterceptor.LOCALE_CONTEXT_KEY.get();
-        return Uni.createFrom().failure(localizationService.getApiException(locale, Status.NOT_FOUND, AUTH_TOKEN_INVALID));
+      Session session = si.getAttribute("session");
+
+      GetSessionResponse.Builder rb = GetSessionResponse.newBuilder().setSid(si.getPrincipal().getName());
+      if (session != null && session.identity != null) {
+        rb.setIid(session.identity.id);
       }
-      return Uni.createFrom().item(GetSessionResponse.newBuilder()
-        .setSid(si.getPrincipal().getName()).build());
+      return Uni.createFrom().item(rb.build());
     });
   }
 }
