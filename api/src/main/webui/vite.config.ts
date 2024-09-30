0eaafddc1c7166e99161de05967c1fd8df7a2f3c
Terrence Curran <tcurran@approachingpi.com>
2024-09-29 23:53:56 -0400

    quinoa, vite, vue

--- api/src/main/webui/vite.config.ts
+++ api/src/main/webui/vite.config.ts
@@ -6,10 +6,17 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
   plugins: [
     vue(),
     vueJsx(),
   ],
+  server: {
+    // this ensures that the browser opens upon server start
+    open: true,
+    // this sets a default port to 3000, you can change this
+    port: 3000,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
