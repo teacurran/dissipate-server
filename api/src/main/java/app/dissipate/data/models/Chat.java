56d7e9190607eaa528811c0178e3f42d218cbbf7
Terrence Curran <tcurran@approachingpi.com>
2024-10-05 16:34:39 -0400

    jpa

--- api/src/main/java/app/dissipate/data/models/Chat.java
+++ api/src/main/java/app/dissipate/data/models/Chat.java
@@ -1,6 +1,7 @@
 package app.dissipate.data.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.NamedQuery;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
@@ -8,12 +9,20 @@ import java.util.List;
 
 @Entity
 @Table(name = "chats")
+@NamedQuery(name = Chat.QUERY_BY_PARTICIPANT, query = """
+    SELECT c
+    FROM Chat c
+    JOIN c.participants p
+    WHERE p.identity.id = :userId
+""")
 public class Chat extends DefaultPanacheEntityWithTimestamps {
+  public static final String QUERY_BY_PARTICIPANT = "Chat.findByParticipant";
 
   @OneToMany(mappedBy = "chat")
   public List<ChatParticipant> participants;
 
   @OneToMany(mappedBy = "chat")
   public List<ChatEvent> events;
+
 
 }
